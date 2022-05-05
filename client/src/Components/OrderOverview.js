0a5d6d2a7fb1b3840e6f02fa629c895510eacfc6
Ethan Freeman <ethanfree23@gmail.com>
2022-05-05 12:06:31 -0500

    orders page light scss

--- client/src/Components/OrderOverview.js
+++ client/src/Components/OrderOverview.js
@@ -4,36 +4,36 @@ import OrderLineItems from '../Components/OrderLineItems'
 
 function OrderOverview() {
     return (
-        <div className="OrderOverview_container__container">
-            <div>
-                <div className="OrderOverview_container__container__header">
-                    <div className="OrderOverview_container__container__title">
-                        Order Overview
-                    </div>
-                    <div className="OrderOverview_container__container__date-range">
-                        Select Dates
-                    </div>
+        <div className="OrderOverview_container">
+            {/* <div> */}
+            <div className="OrderOverview_container__header">
+                <div className="OrderOverview_container__title">
+                    Order Overview
                 </div>
-                <div className="OrderOverview_container__container__options">
-                    <h5>Totals</h5>
-                    <h5>Companies</h5>
-                    <h5>Machine Types</h5>
-                </div>
-                <div className="OrderOverview_container__container__orders-section">
-                    <div className="OrderOverview_container__container__orders-section">
-                        {/* map orders with an order component */}
-                        <table>
-                            <tr>
-                                <th>Company</th>
-                                <th>Part #</th>
-                                <th>Part name</th>
-                                <th>Machine serial #</th>
-                            </tr>
-                            <OrderLineItems />
-                        </table>
-                    </div>
+                <div className="OrderOverview_container__dateRange">
+                    Select Dates
                 </div>
             </div>
+            <div className="OrderOverview_container__options">
+                <h5>Totals</h5>
+                <h5>Companies</h5>
+                <h5>Machine Types</h5>
+            </div>
+            <div className="OrderOverview_container__orders-section">
+                <div className="OrderOverview_container__orders-section">
+                    {/* map orders with an order component */}
+                    <table>
+                        <tr>
+                            <th>Company</th>
+                            <th>Part #</th>
+                            <th>Part name</th>
+                            <th>Machine serial #</th>
+                        </tr>
+                        <OrderLineItems />
+                    </table>
+                </div>
+            </div>
+            {/* </div> */}
         </div>
     )
 }
