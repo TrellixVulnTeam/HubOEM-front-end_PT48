859a72c23e84073bbfab6bcc19e8de8066e10eba
Ethan Freeman <ethanfree23@gmail.com>
2022-05-06 13:47:52 -0500

    edits

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -5,13 +5,15 @@ import { useNavigate } from 'react-router-dom'
 function HomePage() {
   let navigate = useNavigate();
 
-  function placeOrder() {
+  function goToDashboard() {
     navigate('/dashboard')
   }
-  
+
   return (
     <div>
-      <button className="button__green"onClick={placeOrder}>Dashboard</button>
+      <div>this is the <h1>Home Page</h1></div>
+      <h3>go to the <button className="button__green" onClick={goToDashboard}>Dashboard</button> </h3>
+
     </div>
   )
 }
