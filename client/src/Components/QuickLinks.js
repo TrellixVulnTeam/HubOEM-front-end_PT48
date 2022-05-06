c8d268d940aa7c0e506a3684a63257d5bebd24cd
Ethan Freeman <ethanfree23@gmail.com>
2022-05-06 13:08:18 -0500

    working on links on dashboard

--- client/src/Components/QuickLinks.js
+++ client/src/Components/QuickLinks.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function QuickLinks({ title }) {
     return (
-        <div className="Dashboard__quick-links-section">
-            <div className="Dashboard__quick-links-section__container">{title}</div>
+        <div className="QuickLinks">
+            <div className="QuickLinks__items">{title}</div>
         </div>
     )
 }
